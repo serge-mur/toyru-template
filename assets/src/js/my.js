14397f9931080f84d3284e906c464c248a4dcb00
Sergei <serge-mur@yandex.ru>
2019-06-26 16:38:37 +0300

    pre relise main page

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -68,23 +68,37 @@ $(".btn-group, .dropdown").hover(
     function () {
         $('>.dropdown-menu', this).stop(true, true).fadeIn("fast");
         $(this).addClass('open');
+        console.log('addClass open');
     },
     function () {
         $('>.dropdown-menu', this).stop(true, true).fadeOut("fast");
         $(this).removeClass('open');
+        console.log('removeClass open');
     });
 
 
-
 $(document).ready(function () {
+
+    // mobile search
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });  
+
 
     $('.main-slider').slick({
 
     });
 
     $('.brand-slider').slick({
-        slidesToShow: 5,
-        slidesToScroll: 5,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         responsive: [
             {
                 breakpoint: 992,
@@ -112,7 +126,7 @@ $(document).ready(function () {
     $('.char-slider').slick({
         slidesToShow: 5,
         slidesToScroll: 5,
-        centerMode: true,
+        // centerMode: true,
         responsive: [
             {
                 breakpoint: 992,
